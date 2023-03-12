6811614088837825c86c1a2191a0d2e29e688ec8
Anshul BANSAL <smartdiscover17@gmail.com>
2023-03-12 17:28:25 +0200

    spring jdbc mybatis AuthorMapper

--- spring_data_jdbc/src/main/java/com/smartdiscover/mapper/AuthorMapper.java
+++ spring_data_jdbc/src/main/java/com/smartdiscover/mapper/AuthorMapper.java
@@ -1,6 +1,6 @@
 package com.smartdiscover.mapper;
 
-import com.smartdiscover.entity.Book;
+import com.smartdiscover.entity.Author;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -10,7 +10,7 @@ import org.apache.ibatis.annotations.Select;
 public interface AuthorMapper {
 
     @Select("SELECT * FROM AUTHOR WHERE id = #{id}")
-    Book getAuthor(@Param("id") Long id);
+    Author getAuthor(@Param("id") Long id);
 
     @Insert("INSERT INTO AUTHOR (first_name, last_name) VALUES (#{firstName}, #{lastName})")
     Long saveAuthor(@Param("firstName") String firstName, @Param("lastName") String lastName);
