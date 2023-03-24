cae38b36819e9d206f497ae5377de4714f632ec0
Anshul BANSAL <smartdiscover17@gmail.com>
2023-03-24 16:02:29 +0200

    removed the method with target

--- spring_data/src/main/java/com/smartdiscover/entity/projection/PersonInfo.java
+++ spring_data/src/main/java/com/smartdiscover/entity/projection/PersonInfo.java
@@ -1,12 +1,7 @@
 package com.smartdiscover.entity.projection;
 
-import org.springframework.beans.factory.annotation.Value;
-
 public interface PersonInfo {
 
     String getFirstName();
-
-    @Value("#{target.lastName + ' ' + target.firstName}")
-    String getFullName();
 
 }
