82341548d26e1cd61e24e0354e016137869de609
anshulbansal <smartdiscover17@gmail.com>
2023-06-05 22:35:16 +0200

    MongoRepository

--- spring_data_mongodb/src/main/java/com/smartdiscover/repository/BookRepository.java
+++ spring_data_mongodb/src/main/java/com/smartdiscover/repository/BookRepository.java
@@ -1,7 +1,7 @@
 package com.smartdiscover.repository;
 
 import com.smartdiscover.document.Book;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface BookRepository extends CrudRepository<Book, String> {
+public interface BookRepository extends MongoRepository<Book, String> {
 }
